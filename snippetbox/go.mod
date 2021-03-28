2018853174558d8e22675da9fe3c6e91cbe05009
mrqiwi <mrqiwi@ya.ru>
2021-03-28 12:12:54 +0300

    add snippetbox application

---
+++ snippetbox/go.mod
@@ -0,0 +1,12 @@
+module snippetbox
+
+go 1.16
+
+require (
+	github.com/bmizerany/pat v0.0.0-20170815010413-6226ea591a40
+	github.com/go-sql-driver/mysql v1.5.0
+	github.com/golangcollege/sessions v1.2.0
+	github.com/justinas/alice v1.2.0 // indirect
+	github.com/justinas/nosurf v1.1.1 // indirect
+	golang.org/x/crypto v0.0.0-20210322153248-0c34fe9e7dc2
+)
---
+++ snippetbox/pkg/models/models.go
@@ -0,0 +1,27 @@
+package models
+
+import (
+    "time"
+    "errors"
+)
+var (
+    ErrNoRecord = errors.New("models: no matching record found")
+    ErrInvalidCredentials = errors.New("models: invalid credentials")
+    ErrDuplicateEmail = errors.New("models: duplicate email")
+)
+
+type Snippet struct {
+    ID int
+    Title string
+    Content string
+    Created time.Time
+    Expires time.Time
+}
+
+type User struct {
+    ID int
+    Name string
+    Email string
+    HashedPassword []byte
+    Created time.Time
+}
